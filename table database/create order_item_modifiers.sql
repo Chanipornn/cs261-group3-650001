1bb1039ad65002765f3e5893f9350a31c482ebc9
Your Name <natcha.kanch@dome.tu.ac.th>
2025-10-13 20:30:21 +0700

    create order_item_modifiers_db

---
+++ table database/create order_item_modifiers.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE order_item_modifiers (
+    order_item_modifier_id INT IDENTITY(1,1) PRIMARY KEY,
+    order_item_id INT NOT NULL,  -- เชื่อมกับ order_items
+    modifier_id INT NOT NULL,  -- เชื่อมกับ modifiers
+    FOREIGN KEY (order_item_id) REFERENCES order_items(order_item_id),
+    FOREIGN KEY (modifier_id) REFERENCES modifiers(modifier_id)
+);
