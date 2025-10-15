e5c9b5fc493f69bf82efd7d600685eb9254784b1
Your Name <natcha.kanch@dome.tu.ac.th>
2025-10-15 14:54:44 +0700

    Modified menu table

--- table database/table/create menu.sql
+++ table database/table/create menu.sql
@@ -1,6 +1,7 @@
-SELECT TOP (1000) [menu_id]
-      ,[name]
-      ,[category_id]
-      ,[price]
-      ,[allow_notes]
-  FROM [pos_system].[dbo].[menu]
+﻿CREATE TABLE menu (
+    menu_id INT PRIMARY KEY,               
+    name NVARCHAR(255) NOT NULL,           -- ชื่อเมนู
+    category_id INT NOT NULL,              -- หมวดหมู่ (1=อาหาร, 2=เครื่องดื่ม, 3=ของทานเล่น)
+    price DECIMAL(10,2) NOT NULL,          -- ราคา
+    FOREIGN KEY (category_id) REFERENCES menu_categories(category_id)
+);
