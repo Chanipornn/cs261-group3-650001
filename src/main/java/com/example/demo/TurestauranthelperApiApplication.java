795cbe3757323853b7184b5ddd7381972e1cca0a
thanawat-dev <thanawat.kula@dome.tu.ac.th>
2025-10-16 20:54:59 +0700

    fix com.example.demo files

--- src/main/java/com/example/demo/TurestauranthelperApiApplication.java
+++ src/main/java/com/example/demo/TurestauranthelperApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TurestauranthelperApi1Application {
+public class TurestauranthelperApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TurestauranthelperApi1Application.class, args);
+		SpringApplication.run(TurestauranthelperApiApplication.class, args);
 	}
 
 }
