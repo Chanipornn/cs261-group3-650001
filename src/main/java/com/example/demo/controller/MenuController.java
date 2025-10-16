03c2ee71329f5f9b4fe550b69acc2d7ae93117e4
thanawat-dev <thanawat.kula@dome.tu.ac.th>
2025-10-16 22:44:01 +0700

    rebuild application.properties and add controller template

---
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+public class MenuController {
+    }
