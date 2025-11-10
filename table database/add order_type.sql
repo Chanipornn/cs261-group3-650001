aa1940e8f8a3d18c655b2abfe6ea88807e9e52a5
Your Name <natcha.kanch@dome.tu.ac.th>
2025-11-11 00:19:21 +0700

    Add add order_type&add trigger best seller

---
+++ table database/add order_type.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO dbo.order_type (order_type_name)
+VALUES 
+(N'ทานที่ร้าน', 1),  -- ประเภททานที่ร้าน
+(N'กลับบ้าน', 2);    -- ประเภทกลับบ้าน
---
+++ table database/add trigger best seller.sql
@@ -0,0 +1,23 @@
+﻿CREATE TRIGGER trg_update_best_seller
+ON dbo.order_item
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+    -- ลบข้อมูลใน best_seller ที่ไม่ถูกต้อง
+    DELETE FROM dbo.best_seller WHERE menu_id IN (SELECT menu_id FROM inserted);
+
+    -- คำนวณยอดขายรวมของเมนูใหม่ที่มีการเพิ่ม/แก้ไข
+    INSERT INTO dbo.best_seller (menu_id, menu_name, total_sales, total_sales_amount, last_updated)
+    SELECT 
+        oi.menu_id, 
+        m.name AS menu_name, 
+        SUM(oi.quantity) AS total_sales, 
+        SUM(oi.quantity * (m.price + ISNULL(oi.additional_price, 0))) AS total_sales_amount, 
+        GETDATE() AS last_updated
+    FROM 
+        dbo.order_item oi
+    JOIN 
+        dbo.menu m ON oi.menu_id = m.menu_id
+    GROUP BY 
+        oi.menu_id, m.name;
+END;
